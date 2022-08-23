7d7de228da8b228bcdf7e68984aa06ff7b4aae1f
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-23 06:27:59 -0700

    step six, added more items using newItem, renamed sheild asset to shield

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ let pillar = newImage('pillar.png', 350, 100)
 let crate = newImage('crate.png', 150, 200)
 let well = newImage('well.png', 500, 425)
 
-
 function newItem(imgSrc, leftPosition, bottomPosition) {
     let item = newImage(imgSrc, leftPosition, bottomPosition)
 
@@ -25,3 +24,5 @@ function newItem(imgSrc, leftPosition, bottomPosition) {
 }
 
 let sword = newItem('sword.png', 500, 405)
+let shield = newItem('shield.png', 165, 185)
+let staff = newItem('staff.png', 600, 100)

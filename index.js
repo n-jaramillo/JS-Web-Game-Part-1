965ff22b9ba6edf2f44e6b05feee26ce098ae172
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-23 06:14:51 -0700

    step three, refactored adding a new image with a function

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(imgSrc, leftPosition, bottomPosition) {
+    let image = document.createElement('img')
+    image.src = 'assets/' + imgSrc
+    image.style.position = 'fixed'
+    image.style.left = leftPosition + 'px'
+    image.style.bottom = bottomPosition + 'px'
+    document.body.append(image)    
+    return image
+}
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+let greenCharacter = newImage('green-character.gif', 100, 100)
+
+let pineTree = newImage('pine-tree.png', 450, 200)
